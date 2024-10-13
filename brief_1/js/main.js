112b4e63bdb434a846a40faf99efed2aed454410
MOHSEN ELRHAZI <mohsenelrhazi3@gmail.com>
2024-10-13 15:19:29 +0100

    ameliorer le brief 1

--- brief_1/js/main.js
+++ brief_1/js/main.js
@@ -1,33 +1,5 @@
-// $('.owl-carousel').owlCarousel({
-//     loop:true,
-//     margin:10,
-//     nav:true,
-//     responsive:{
-//         0:{
-//             items:1
-//         },
-//         600:{
-//             items:3
-//         },
-//         1000:{
-//             items:5
-//         }
-//     }
-// })
 
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
-        loop: true,
-        items: 1,
-        nav: true,
-        // autoplay: true,
-        autoplayTimeout:5000,
-    });
-});
-  
- 
-// ! our partners
-
+// ! Que nous sommes
 $(document).ready(function(){
     $('.customer-logos').slick({
         slidesToShow: 6,
